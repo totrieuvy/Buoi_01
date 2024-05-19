ccd7ce906c972dc6a36fa5f6e11f9dab54d25fa6
= <=>
2024-05-19 11:47:23 +0700

    MyComponent

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MyComponent from "./MyComponent/MyComponent";
+
+function App() {
+  return (
+    <div>
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyComponent/MyComponent.jsx
@@ -0,0 +1,9 @@
+function MyComponent() {
+  return (
+    <div>
+      <h3>This is my component</h3>
+    </div>
+  );
+}
+
+export default MyComponent;
